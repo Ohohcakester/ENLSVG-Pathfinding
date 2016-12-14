5a81875d90fef39334b3c80e20b1b3e1dd3f3f4d
Ohohcakester <ohohgit@gmail.com>
2016-12-14 12:39:31 +0800

    Generate random grid in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
 #include "Grid.h"
 #include "Drawer.h"
+#include "RandomGridGenerator.h"
 
 int main() {
-    int sizeX = 100;
-    int sizeY = 100;
+    int sizeX = 200;
+    int sizeY = 200;
 
     Grid grid(sizeX, sizeY);
-    grid.initialiseRandomGrid(0.5f);
-    
+    //RandomGridGenerator::generateRandomGrid(grid, 0.5f);
+    RandomGridGenerator::generateAutomataGrid(grid, 0.5f, 5, 1.f);
+
     Drawer::drawGrid(grid, "outputGrid.tga");
 }
