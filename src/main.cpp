7a2cc561ec7c55589e773500ef8dcc953e1542bb
Ohohcakester <ohohgit@gmail.com>
2016-12-14 11:20:25 +0800

    grid drawing code

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 #include "Grid.h"
+#include "Drawer.h"
 
 int main() {
     int sizeX = 100;
     int sizeY = 100;
 
     Grid grid(sizeX, sizeY);
+    grid.initialiseRandomGrid(0.5f);
+    
+    Drawer::drawGrid(grid, "outputGrid.tga");
 }
