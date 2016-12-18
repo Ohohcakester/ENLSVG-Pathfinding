f5d5fed6bc22992168a76cdba777606dcfcc0d81
Ohohcakester <ohohgit@gmail.com>
2016-12-18 10:42:40 +0800

    Fix indentation in Image.cpp

--- src/Image.cpp
+++ src/Image.cpp
@@ -6,76 +6,73 @@
  */
 
 //Default Constructor
-TGAImage::TGAImage() {
-
-}
+TGAImage::TGAImage() {}
 
 //Overridden Constructor
 TGAImage::TGAImage(short width, short height) {
-	m_width = width;
-	m_height = height;
-	m_pixels = new Colour[m_width*m_height];
+    m_width = width;
+    m_height = height;
+    m_pixels = new Colour[m_width*m_height];
 }
 
 //Set all pixels at once
 void TGAImage::setAllPixels(Colour *pixels) {
-	m_pixels = pixels;
+    m_pixels = pixels;
 }
 
 //Set indivdual pixels
 void TGAImage::setPixel(Colour inputcolor, int x, int y) {
-  m_pixels[convert2dto1d(x,y)] = inputcolor;
+    m_pixels[convert2dto1d(x,y)] = inputcolor;
 }
 
 //set individual pixels, includes boundary check.
 void TGAImage::setPixelSafe(Colour inputcolor, int x, int y) {
-  if (x < 0 || y < 0 || x >= m_width || y >= m_height) return;
-  m_pixels[convert2dto1d(x,y)] = inputcolor;
+    if (x < 0 || y < 0 || x >= m_width || y >= m_height) return;
+    m_pixels[convert2dto1d(x,y)] = inputcolor;
 }
 
 //Convert 2d array indexing to 1d indexing
 int TGAImage::convert2dto1d(int x, int y) {
-	return m_width * y + x;
+	 return m_width * y + x;
 }
 
 void TGAImage::WriteImage(string filename) {
 
-	//Error checking
-	if (m_width <= 0 || m_height <= 0)
-	{
-		cout << "Image size is not set properly" << endl;
-		return;
-	}
+    //Error checking
+    if (m_width <= 0 || m_height <= 0)
+    {
+        cout << "Image size is not set properly" << endl;
+        return;
+    }
 
-	ofstream o(filename.c_str(), ios::out | ios::binary);
+    ofstream o(filename.c_str(), ios::out | ios::binary);
 
-	//Write the header
-	o.put(0);
-   	o.put(0);
-   	o.put(2);                         /* uncompressed RGB */
-   	o.put(0); 		o.put(0);
-   	o.put(0); 	o.put(0);
-   	o.put(0);
-   	o.put(0); 	o.put(0);           /* X origin */
-   	o.put(0); 	o.put(0);           /* y origin */
+    //Write the header
+    o.put(0);
+    o.put(0);
+    o.put(2);                         /* uncompressed RGB */
+    o.put(0); 		o.put(0);
+    o.put(0); 	o.put(0);
+    o.put(0);
+    o.put(0); 	o.put(0);           /* X origin */
+    o.put(0); 	o.put(0);           /* y origin */
     o.put((m_width & 0x00FF));
     o.put((m_width & 0xFF00) / 256);
-   	o.put((m_height & 0x00FF));
-   	o.put((m_height & 0xFF00) / 256);
-   	o.put(32);                        /* 24 bit bitmap */
-   	o.put(0);
-   	
-	//Write the pixel data
-	for (int i=0;i<m_width*m_height;i++) {
-		o.put(m_pixels[i].b);
-		o.put(m_pixels[i].g);
-		o.put(m_pixels[i].r);
-		o.put(m_pixels[i].a);
-	}   
-	
-	//close the file
-	o.close();
-	
+    o.put((m_height & 0x00FF));
+    o.put((m_height & 0xFF00) / 256);
+    o.put(32);                        /* 24 bit bitmap */
+    o.put(0);
+
+    //Write the pixel data
+    for (int i=0;i<m_width*m_height;i++) {
+        o.put(m_pixels[i].b);
+        o.put(m_pixels[i].g);
+        o.put(m_pixels[i].r);
+        o.put(m_pixels[i].a);
+    }   
+
+    //close the file
+    o.close();
 }
 
 namespace Colours {
