023f7f465e7538333e8fb23a44db62047b028f80
Ohohcakester <ohohgit@gmail.com>
2016-12-19 10:25:12 +0800

    ENLSVGEdgeGraph.h. Placeholder implementation of ENLSVG hierarchy building. Compiles but completely untested.

---
+++ src/ENLSVGEdgeGraph.cpp
@@ -0,0 +1,235 @@
+#include "ENLSVGEdgeGraph.h"
+#include "LineOfSightScanner.h"
+#include "Grid.h"
+#include <algorithm>
+
+ENLSVGEdgeGraph::ENLSVGEdgeGraph(const Grid& grid, const LineOfSightScanner& scanner):
+    grid(grid), sizeX(grid.sizeX), sizeY(grid.sizeY), scanner(scanner) {
+
+    // Initialise vertices (outer corners).
+    nodeIndexes.resize(sizeY+1);
+    for (int y=0;y<=sizeY;++y) nodeIndexes[y].resize(sizeX+1, -1);
+    for (int y=0;y<=sizeY;++y) {
+        for (int x=0;x<=sizeX;++x) {
+            if (grid.isOuterCorner(x,y)) {
+                nodeIndexes[y][x] = vertices.size();
+                vertices.push_back(GridVertex(x,y));
+            }
+        }
+    }
+
+    // Initialise SVG edges
+    edgeLists.resize(vertices.size());
+    ScannerStacks scannerStacks;
+    for (size_t i=0;i<vertices.size();++i) {
+        int cx = vertices[i].x;
+        int cy = vertices[i].y;
+        scanner.computeTautDirNeighbours(scannerStacks, cx, cy);
+        std::vector<GridVertex>& neighbours = scannerStacks.neighbours;
+
+        for (size_t j=0;j<neighbours.size();++j) {
+            int nx = neighbours[j].x;
+            int ny = neighbours[j].y;
+
+            VertexID dest = nodeIndexes[ny][nx];
+            if (dest >= i) continue;
+
+            connectEdge(i, dest, cx, cy, nx, ny);
+        }
+    }
+
+    // Connect Taut Neighbours of Edges
+    for (size_t i=0; i<edges.size(); ++i) {
+        ENLSVGEdge& edge = edges[i];
+        std::vector<EdgeID>& tautOutgoingEdges = edge.tautOutgoingEdges;
+        VertexID src = edge.sourceVertex;
+        VertexID dest = edge.destVertex;
+        int sx = vertices[src].x;
+        int sy = vertices[src].y;
+        int ex = vertices[dest].x;
+        int ey = vertices[dest].y;
+
+        const std::vector<EdgeID>& outgoingEdges = edgeLists[dest];
+        for (size_t j=0; j<outgoingEdges.size(); ++j) {
+            EdgeID succ = outgoingEdges[j];
+            // assert edges[succ].sourceVertex == dest
+            VertexID next = edges[succ].destVertex;
+            if (next == src) continue;
+
+            int nx = vertices[next].x;
+            int ny = vertices[next].y;
+            if (grid.isTaut(sx, sy, ex, ey, nx, ny)) {
+                tautOutgoingEdges.push_back(succ);
+            }
+        }
+    }
+
+    buildHierarchy();
+}
+
+void ENLSVGEdgeGraph::connectEdge(int i, int j, int xi, int yi, int xj, int yj) {
+    double weight = grid.euclideanDistance(xi,yi,xj,yj);
+
+    EdgeID edge_ij = edges.size();
+    edges.push_back(ENLSVGEdge(i, j, weight, LEVEL_W));
+
+    EdgeID edge_ji = edges.size();
+    edges.push_back(ENLSVGEdge(j, i, weight, LEVEL_W));
+
+    edges[edge_ij].oppositeEdge = edge_ji;
+    edges[edge_ji].oppositeEdge = edge_ij;
+
+    edgeLists[i].push_back(edge_ij);
+    edgeLists[j].push_back(edge_ji);
+}
+
+void ENLSVGEdgeGraph::buildHierarchy() {
+    computeAllEdgeLevels();
+    setupSkipEdges();
+}
+
+void ENLSVGEdgeGraph::computeAllEdgeLevels() {
+
+    std::vector<EdgeID> currentLevelEdges;
+    std::vector<EdgeID> nextLevelEdges;
+    
+    std::vector<int> nNeighbours;
+    nNeighbours.resize(edges.size());
+    for (EdgeID i=0; i<edges.size(); ++i) {
+        if (edges[i].isOriginal()) {
+            int n = edges[i].tautOutgoingEdges.size() + edges[opposite(i)].tautOutgoingEdges.size();
+            nNeighbours[i] = n;
+            if (n == 0) currentLevelEdges.push_back(i);
+        }
+    }
+
+    int currLevel = 1;
+    while (currentLevelEdges.size() > 0) {
+        for (size_t i=0;i<currentLevelEdges.size(); ++i) {
+            EdgeID curr = currentLevelEdges[i];
+            EdgeID opp = opposite(curr);
+
+            edges[curr].level = currLevel;
+            edges[opp].level = currLevel;
+
+            const std::vector<EdgeID>& neighbours1 = edges[curr].tautOutgoingEdges;
+            for (size_t j=0; j<neighbours1.size(); ++j) {
+                EdgeID neighbour = getOriginal(neighbours1[j]);
+                if (edges[neighbour].level != LEVEL_W) continue;
+                
+                --nNeighbours[neighbour];
+                if (nNeighbours[neighbour] == 0) {
+                    nextLevelEdges.push_back(neighbour);
+                }
+                // debugging
+                if (nNeighbours[neighbour] < 0) std::cout << "ERROR" << std::endl;
+            }
+
+            const std::vector<EdgeID>& neighbours2 = edges[opp].tautOutgoingEdges;
+            for (size_t j=0; j<neighbours2.size(); ++j) {
+                EdgeID neighbour = getOriginal(neighbours2[j]);
+                if (edges[neighbour].level != LEVEL_W) continue;
+                
+                --nNeighbours[neighbour];
+                if (nNeighbours[neighbour] == 0) {
+                    nextLevelEdges.push_back(neighbour);
+                }
+                // debugging
+                if (nNeighbours[neighbour] < 0) std::cout << "ERROR" << std::endl;
+            }
+        }
+        currentLevelEdges.clear();
+        std::swap(currentLevelEdges, nextLevelEdges);
+        ++currLevel;
+    }
+}
+
+void ENLSVGEdgeGraph::setupSkipEdges() {
+    std::vector<VertexID> skipVertices;
+    std::vector<bool> isSkipVertex;
+    isSkipVertex.resize(vertices.size(), false);
+
+    for (VertexID i=0; i<vertices.size(); ++i) {
+        const auto& edgeList = edgeLists[i];
+        int nLevelWEdges = 0;
+        for (size_t j=0; j<edgeList.size(); ++j) {
+            if (edges[edgeList[j]].level == LEVEL_W) {
+                ++nLevelWEdges;
+                if (nLevelWEdges >= 3) {
+                    isSkipVertex[i] = true;
+                    skipVertices.push_back(i);
+                    break;
+                }
+            }
+        }
+    }
+
+    for (size_t i=0; i<skipVertices.size(); ++i) {
+        VertexID curr = skipVertices[i];
+        const auto& edgeList = edgeLists[curr];
+
+        for (size_t j=0; j<edgeList.size(); ++j) {
+            if (edges[edgeList[j]].level != LEVEL_W) continue;
+            // follow edge till next skip vertex.
+            double totalWeight;
+            VertexID nextVertex;
+            followLevelWPathToNextSkipVertex(edgeList[j], totalWeight, nextVertex, isSkipVertex);
+            skipEdges[curr].push_back(SkipEdge(nextVertex, totalWeight));
+        }
+    }
+}
+
+void ENLSVGEdgeGraph::followLevelWPathToNextSkipVertex(EdgeID firstEdge, double& totalWeight, VertexID& nextVertex, const std::vector<bool>& isSkipVertex) const {
+    EdgeID currEdge = firstEdge;
+    totalWeight = edges[currEdge].weight; // RETURN VALUE
+    while (!isSkipVertex[edges[currEdge].destVertex]) {
+        const auto& tautOutgoingEdges = edges[currEdge].tautOutgoingEdges;
+
+        // Find next outgoing level-W edge.
+        for (size_t i=0; i<tautOutgoingEdges.size(); ++i) {
+            EdgeID nextEdge = tautOutgoingEdges[i];
+            if (edges[nextEdge].level == LEVEL_W) {
+                currEdge = nextEdge;
+                break;
+            }
+        }
+        totalWeight += edges[currEdge].weight;
+        // This should not infinite loop done correctly.
+    }
+    nextVertex = edges[currEdge].destVertex; // RETURN VALUE
+}
+
+void ENLSVGEdgeGraph::markEdgesFrom(MarkedEdges& markedEdges, const int sx, const int sy, const std::vector<int>& neighbours) const {
+    std::vector<EdgeID> edgeQueue;
+    size_t i = 0;
+    
+    while (i < edgeQueue.size()) {
+        EdgeID curr = edgeQueue[i];
+
+        const int currLevel = edges[curr].level;
+        const auto& tautOutgoingEdges = edges[curr].tautOutgoingEdges;
+        bool skipVertex = isSkipVertex(edges[curr].destVertex);
+
+        for (size_t j=0; j<tautOutgoingEdges.size(); ++j) {
+            EdgeID next = tautOutgoingEdges[j];
+            if (markedEdges.isMarked[next]) continue;
+
+            if (edges[next].level > currLevel || (!skipVertex && edges[next].level == LEVEL_W)) {
+                markedEdges.mark(next);
+                edgeQueue.push_back(next);
+            }
+        }
+
+        ++i;
+    }
+}
+
+// Duplicate all markings. Call this after calling markEdgesFrom from both start and goal.
+void ENLSVGEdgeGraph::markBothWays(MarkedEdges& markedEdges) const {
+    std::vector<EdgeID>& markedIndexes = markedEdges.markedIndexes;
+    size_t nMarked = markedIndexes.size(); // freeze size.
+    for (size_t i=0;i<nMarked;++i) {
+        EdgeID opp = opposite(markedIndexes[i]);
+        if (!markedEdges.isMarked[opp]) markedEdges.mark(opp);
+    }
+}
---
+++ src/ENLSVGEdgeGraph.h
@@ -0,0 +1,93 @@
+#ifndef _ENLSVG_EDGE_GRAPH_H_
+#define _ENLSVG_EDGE_GRAPH_H_
+
+#include "PathfindingDataTypes.h"
+#include <iostream>
+#include <vector>
+class Grid;
+class LineOfSightScanner;
+typedef int VertexID;
+typedef size_t EdgeID;
+
+struct ENLSVGEdge {
+    EdgeID oppositeEdge;
+    VertexID sourceVertex;
+    VertexID destVertex;
+    double weight;
+    int level;
+    std::vector<EdgeID> tautOutgoingEdges;
+
+    inline bool isOriginal() const {return sourceVertex < destVertex;}
+
+    ENLSVGEdge(VertexID sourceVertex, VertexID destVertex, double weight, int level)
+    : sourceVertex(sourceVertex), destVertex(destVertex), weight(weight), level(level) {}
+};
+
+struct SkipEdge {
+    VertexID next;
+    double weight;
+
+    SkipEdge(VertexID next, double weight): next(next), weight(weight) {}
+};
+
+struct MarkedEdges {
+    std::vector<bool> isMarked;
+    std::vector<EdgeID> markedIndexes;
+
+    MarkedEdges(size_t nEdges) {
+        isMarked.resize(nEdges, false);
+    }
+
+    inline void mark(EdgeID index) {
+        isMarked[index] = true;
+        markedIndexes.push_back(index);
+    }
+
+    inline void clear() {
+        for (size_t i=0;i<markedIndexes.size();++i) {
+            isMarked[i] = false;
+        }
+        markedIndexes.clear();
+    }
+};
+
+class ENLSVGEdgeGraph {
+
+public:
+    ENLSVGEdgeGraph(const Grid& grid, const LineOfSightScanner& scanner);
+    void markEdgesFrom(MarkedEdges& markedEdges, const int sx, const int sy, const std::vector<int>& neighbours) const;
+    void markBothWays(MarkedEdges& markedEdges) const;
+
+    const int LEVEL_W = -1;
+    const int sizeX;
+    const int sizeY;
+
+    // Indexed by VertexID
+    std::vector<GridVertex> vertices;
+    std::vector<std::vector<EdgeID>> edgeLists; // points to edge indexes.
+    std::vector<std::vector<SkipEdge>> skipEdges;
+
+    // Indexed by grid coordinates.
+    std::vector<std::vector<VertexID>> nodeIndexes;
+
+    // Indexed by EdgeID
+    std::vector<ENLSVGEdge> edges;
+    
+private:
+    const Grid& grid;
+    const LineOfSightScanner& scanner;
+
+    inline EdgeID opposite(EdgeID edgeID) const {return edges[edgeID].oppositeEdge;}
+    inline EdgeID getOriginal(EdgeID edgeID) const {return edges[edgeID].isOriginal() ? edgeID : edges[edgeID].oppositeEdge;}
+    inline bool isSkipVertex(VertexID vertexID) const {return skipEdges[vertexID].size() > 0;}
+
+    void connectEdge(int i, int j, int xi, int yi, int xj, int yj);
+    void buildHierarchy();
+    void computeAllEdgeLevels();
+    void setupSkipEdges();
+    void followLevelWPathToNextSkipVertex(EdgeID firstEdge, double& totalWeight, VertexID& nextVertex, const std::vector<bool>& isSkipVertex) const;
+};
+
+
+
+#endif
