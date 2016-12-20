75e8feee16e368368cc622027ca5b05fc9585d16
Ohohcakester <ohohgit@gmail.com>
2016-12-20 13:43:33 +0800

    Make main.cpp run the ENLSVG algorithm

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 #include "Grid.h"
 #include "Drawer.h"
 #include "RandomGridGenerator.h"
-#include "ENLSVGAlgorithm.h"
+#include "ENLSVGEdgeAlgorithm.h"
 #include "PathfindingDataTypes.h"
 
 int main() {
@@ -13,18 +13,18 @@ int main() {
     //RandomGridGenerator::generateRandomGrid(grid, 0.5f);
     RandomGridGenerator::generateAutomataGrid(grid, 0.5f, 5, 1.f);
 
-    ENLSVGAlgorithm enlsvg(grid);
+    ENLSVGEdgeAlgorithm enlsvg(grid);
 
-    const int sx = 31;
-    const int sy = 12;
-    const int ex = 179;
-    const int ey = 17;
+    const int sx = 152;
+    const int sy = 72;
+    const int ex = 53;
+    const int ey = 95;
     ParentPtrs parentPtrs;
-    Path path = enlsvg.computeSVGPath(sx, sy, ex, ey, &parentPtrs);
+    Path path = enlsvg.computePath(sx, sy, ex, ey, &parentPtrs);
 
     Drawer drawer(grid, 5);
     drawer.drawGrid(grid);
-    drawer.drawVisibilityGraph(enlsvg.graph);
+    //drawer.drawVisibilityGraph(enlsvg.graph);
     drawer.drawParentPointers(parentPtrs);
     drawer.drawPath(path);
     drawer.drawPoint(sx, sy);
